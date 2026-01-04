88ea9e3a7e9cd0121ad999dfcdd9c2a3c1984803
rocjay1 <jasonroc19@gmail.com>
2026-01-04 20:33:05 +0000

    Display top 3 'offenders' in stdout

--- health_check.sh
+++ health_check.sh
@@ -1,9 +1,24 @@
 #! /bin/bash
 
+# Set up logging
+
+# Security Audit
+echo -e "|COUNT|IP|\n|---|---|"
+
 # https://askubuntu.com/a/178019
 # Filter for brute-force interactive SSH logins
 # Look for failed connections (i.e. no login attempted, could be a port scanner, etc.)
-awk '/Connection closed by|Failed password/ { 
-    where = match($0, /[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}/)
-    print substr($0, RSTART, RLENGTH)
-}' /var/log/auth.log > >(sort | uniq -c)
+awk '/Connection closed by|Failed password for/ { 
+        i = match($0, /[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}/)
+        m = substr($0, RSTART, RLENGTH)
+        a[m]++
+    } END {
+    for (m in a) {
+        printf("%-15s %s\n", a[m], m)
+    }
+}' /var/log/auth.log | 
+sort -nr | 
+head -n 3 | 
+# Match start of line OR 1 or more spaces OR end of line 
+# and replace with "|"
+sed 's/\(^\)\|\([[:space:]]\+\)\|\($\)/\|/g'
